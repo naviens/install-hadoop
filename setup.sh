ca5117ed2dc1588bbd8bcfe91e78d1944d07680f
Naveen S <naviensubramani@gmail.com>
2014-01-27 18:52:31 +0530

    initial version

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
+echo "------> Installing open SSh Server"
+
+__apt_get_noinput() {
+    apt-get install -y -o DPkg::Options::=--force-confold $@
+}
+apt-get update
+__apt_get_noinput ssh
+apt-get update
+echo "------> Downloading Hadoop 2.2.0 package"
 wget -c http://www.webhostingjams.com/mirror/apache/hadoop/common/stable2/hadoop-2.2.0.tar.gz
+echo "------> Installing Hadoop Package"
 tar -zxf hadoop-2.2.0.tar.gz
 mv hadoop-2.2.0 /home/$USER/hadoop-2.2.0
 cp conf/core-site.xml /home/$USER/hadoop-2.2.0/conf/core-site.xml
