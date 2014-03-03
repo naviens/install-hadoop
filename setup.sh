fc6dcfecaa0c0a7abc79fbd8233d350d3dcb2f8f
Naveen Subramani <naviensubramani@gmail.com>
2014-03-03 20:00:49 +0530

    code clean up

--- setup.sh
+++ setup.sh
@@ -4,20 +4,21 @@ source .bashrc
 mkdir -p $HOME/yarn/yarn_data/hdfs/namenode
 mkdir -p $HOME/yarn/yarn_data/hdfs/datanode
 echo "------> Downloading Hadoop 2.2.0 package"
+YARN_HOME_DIR=/home/$1/yarn/hadoop-2.2.0
 wget -c http://apache.mirrors.hoobly.com/hadoop/common/stable2/hadoop-2.2.0.tar.gz
 echo "------> Installing Hadoop Package"
 tar -xvzf hadoop-2.2.0.tar.gz
-mv hadoop-2.2.0 /home/$1/yarn/hadoop-2.2.0
-chmod -R 755 /home/$1/yarn/hadoop-2.2.0
-cp -rf conf/yarn/core-site.xml /home/$1/yarn/hadoop-2.2.0/etc/hadoop/core-site.xml
-cp -rf conf/yarn/mapred-site.xml /home/$1/yarn/hadoop-2.2.0/etc/hadoop/mapred-site.xml
-cp -rf conf/yarn/yarn-site.xml /home/$1/yarn/hadoop-2.2.0/etc/hadoop/yarn-site.xml
-awk '{gsub("hadoop", "'${1}'", $0); print}' > /home/$1/yarn/hadoop-2.2.0/etc/hadoop/hdfs-site.xml < conf/yarn/hdfs-site.xml
-mkdir /home/$1/yarn/hadoop-2.2.0/input
-cp -rf file /home/$1/yarn/hadoop-2.2.0/input/
-cp conf/stop.sh /home/$1/yarn/hadoop-2.2.0/
-cp conf/start.sh /home/$1/yarn/hadoop-2.2.0/
-cd /home/$1/yarn/hadoop-2.2.0/
+mv hadoop-2.2.0 $YARN_HOME_DIR
+chmod -R 755 $YARN_HOME_DIR
+cp -rf conf/yarn/core-site.xml $YARN_HOME_DIR/etc/hadoop/core-site.xml
+cp -rf conf/yarn/mapred-site.xml $YARN_HOME_DIR/etc/hadoop/mapred-site.xml
+cp -rf conf/yarn/yarn-site.xml $YARN_HOME_DIR/etc/hadoop/yarn-site.xml
+awk '{gsub("hadoop", "'${1}'", $0); print}' > $YARN_HOME_DIR/etc/hadoop/hdfs-site.xml < conf/yarn/hdfs-site.xml
+mkdir $YARN_HOME_DIR/input
+cp -rf file $YARN_HOME_DIR/input/
+cp conf/stop.sh $YARN_HOME_DIR/
+cp conf/start.sh $YARN_HOME_DIR/
+cd $YARN_HOME_DIR/
 bin/hadoop namenode -format
 # HDFS(NameNode & DataNode).
 sbin/hadoop-daemon.sh start namenode
