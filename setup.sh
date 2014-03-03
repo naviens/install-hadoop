341af0196a5d721ceb7ba939444f34c36c6c824a
Naveen S <naviensubramani@gmail.com>
2014-03-03 17:09:52 +0530

    working git

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo "------> Installing open SSh Server"
 
 __apt_get_noinput() {
@@ -7,10 +8,12 @@ apt-get update
 __apt_get_noinput ssh
 apt-get update
 echo "------> Downloading Hadoop 2.2.0 package"
-wget -c http://www.webhostingjams.com/mirror/apache/hadoop/common/stable2/hadoop-2.2.0.tar.gz
+wget -c http://apache.mirrors.hoobly.com/hadoop/common/stable2/hadoop-2.2.0.tar.gz
 echo "------> Installing Hadoop Package"
-tar -zxf hadoop-2.2.0.tar.gz
-mv hadoop-2.2.0 /home/$USER/hadoop-2.2.0
-cp conf/core-site.xml /home/$USER/hadoop-2.2.0/conf/core-site.xml
-cp conf/hdfs-site.xml /home/$USER/hadoop-2.2.0/conf/hdfs-site.xml
-cp conf/mapred-site.xml /home/$USER/hadoop-2.2.0/conf/mapred-site.xml
+tar -xvzf hadoop-2.2.0.tar.gz
+mv hadoop-2.2.0 /home/$USER/hadoop-yarn
+chmod -R 755 /home/$USER/hadoop-yarn
+cp conf/yarn/core-site.xml /home/$USER/hadoop-yarn/etc/hadoop/core-site.xml
+cp conf/yarn/hdfs-site.xml /home/$USER/hadoop-yarn/etc/hadoop/hdfs-site.xml
+cp conf/yarn/mapred-site.xml /home/$USER/hadoop-yarn/etc/hadoop/mapred-site.xml
+cp conf/yarn/yarn-site.xml /home/$USER/hadoop-yarn/etc/hadoop/yarn-site.xml
