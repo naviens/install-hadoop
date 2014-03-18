917c12d34688eb5171cd87cd2b39d96df31d199c
Naveen Subramani <naviensubramani@gmail.com>
2014-03-18 15:26:40 +0530

    bug fix

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ if [ $options -eq 1 ]; then
 	echo "------> Installing Hadoop 2.2.0 Package"
 	tar -xvzf hadoop-2.2.0.tar.gz
 	mv hadoop-2.2.0 $YARN_HOME_DIR	
-elif [ $options -eq 1 ]
+elif [ $options -eq 2 ]
 then
     echo "------> Downloading Hadoop 2.3.0..........."
 	YARN_HOME_DIR=/home/$1/yarn/hadoop-2.3.0
@@ -44,37 +44,37 @@ else
     mv spark-0.9.0-incubating-bin-hadoop2 $HOME_DIR
 fi
 
-if [ $options -eq 1 or $options -eq 2 ]; then
-    chmod -R 755 $YARN_HOME_DIR
-    cp -rf conf/yarn/core-site.xml $YARN_HOME_DIR/etc/hadoop/core-site.xml
-    cp -rf conf/yarn/mapred-site.xml $YARN_HOME_DIR/etc/hadoop/mapred-site.xml
-    cp -rf conf/yarn/yarn-site.xml $YARN_HOME_DIR/etc/hadoop/yarn-site.xml
-    awk '{gsub("hadoop", "'${1}'", $0); print}' > $YARN_HOME_DIR/etc/hadoop/hdfs-site.xml < conf/yarn/hdfs-site.xml
-    mkdir $YARN_HOME_DIR/input
-    cp -rf file $YARN_HOME_DIR/input/
-    cp conf/stop.sh $YARN_HOME_DIR/
-    cp conf/start.sh $YARN_HOME_DIR/
-    cd $YARN_HOME_DIR/
-    bin/hadoop namenode -format
-    # HDFS(NameNode & DataNode).
-    sbin/hadoop-daemon.sh start namenode
-    sbin/hadoop-daemon.sh start datanode
-    # MR(Resource Manager, Node Manager & Job History Server).
-    sbin/yarn-daemon.sh start resourcemanager
-    sbin/yarn-daemon.sh start nodemanager
-    sbin/mr-jobhistory-daemon.sh start historyserver
+chmod -R 755 $YARN_HOME_DIR
+cp -rf conf/yarn/core-site.xml $YARN_HOME_DIR/etc/hadoop/core-site.xml
+cp -rf conf/yarn/mapred-site.xml $YARN_HOME_DIR/etc/hadoop/mapred-site.xml
+cp -rf conf/yarn/yarn-site.xml $YARN_HOME_DIR/etc/hadoop/yarn-site.xml
+awk '{gsub("hadoop", "'${1}'", $0); print}' > $YARN_HOME_DIR/etc/hadoop/hdfs-site.xml < conf/yarn/hdfs-site.xml
+mkdir $YARN_HOME_DIR/input
+cp -rf file $YARN_HOME_DIR/input/
+cp conf/stop.sh $YARN_HOME_DIR/
+cp conf/start.sh $YARN_HOME_DIR/
+cd $YARN_HOME_DIR/
+bin/hadoop namenode -format
+# HDFS(NameNode & DataNode).
+sbin/hadoop-daemon.sh start namenode
+sbin/hadoop-daemon.sh start datanode
+# MR(Resource Manager, Node Manager & Job History Server).
+sbin/yarn-daemon.sh start resourcemanager
+sbin/yarn-daemon.sh start nodemanager
+sbin/mr-jobhistory-daemon.sh start historyserver
 
-    echo "------> Verifying Installation For user "$1
-    jps
+echo "------> Verifying Installation For user "$1
+jps
 
-    echo "---------> Successfully Installed Hadoop on your machine <--------------"
-    echo "---------> waiting for hadoop daemons <---------------"
-    sleep 20
-    echo "Uploading Wordcount program to hdfs....."
-    bin/hadoop dfs -copyFromLocal input /input
-    sleep 5
-    echo "Executing Sample Hello world Example....."
-else
+echo "---------> Successfully Installed Hadoop on your machine <--------------"
+echo "---------> waiting for hadoop daemons <---------------"
+sleep 20
+echo "Uploading Wordcount program to hdfs....."
+bin/hadoop dfs -copyFromLocal input /input
+sleep 5
+echo "Executing Sample Hello world Example....."
+
+if [ $options -eq 3 ]; then
     echo "Installing Spark....."
     cd $HOME_DIR/spark-0.9.0-incubating-bin-hadoop2
     sbin/start-all.sh
